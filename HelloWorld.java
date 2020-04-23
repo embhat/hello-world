ddd884c447ac3a49a0382cc3d6fc16c893aa580a
embhat <embhat@gmail.com>
2020-04-23 16:21:04 +0530

    comments updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 /** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ * Hello World class implements an application that prints "Hello World" to standard output.
  */
 public class HelloWorld {
   public static void main(String[] args) {
